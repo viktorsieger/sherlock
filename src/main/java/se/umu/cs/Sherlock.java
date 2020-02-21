d0bfa3455e45be24ed1c1a4bd1dc8d1f8454279c
viktorsieger <23069803+viktorsieger@users.noreply.github.com>
2020-02-21 07:03:39 +0100

    Updated POM, .gitignore and created Sherlock.java

---
+++ src/main/java/se/umu/cs/Sherlock.java
@@ -0,0 +1,14 @@
+package se.umu.cs;
+
+public class Sherlock {
+    public static void main(String[] args) {
+
+	if(args.length != 1) {
+	    throw new IllegalArgumentException();
+	}
+
+	System.out.println("TESTTT");
+
+	return;
+    }
+}
